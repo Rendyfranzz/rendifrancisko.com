32173f384eb3c8f76cd958cc47d11db81d7be999
rendi <rendicrew96@gmail.com>
2026-01-10 10:18:51 +0700

    fix: update copyright year in footer to 2026

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -51,7 +51,7 @@ export const Footer = () => {
             Tailwind CSS
           </LinkPreview>
         </div>
-        <p className=''>&copy; Rendi Dwi Francisko 2025</p>
+        <p className=''>&copy; Rendi Dwi Francisko 2026</p>
         {/* <Spotify className='mt-8' /> */}
       </div>
     </footer>
